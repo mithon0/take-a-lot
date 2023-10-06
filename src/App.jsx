c214222ea3934f9da4c9b96c050dd88e291bc09c
dev-talukdar <dev.talukdaar@gmail.com>
2023-10-06 11:15:12 +0600

    merge issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
-function App() {
 
+
+function App() {
 
   return (
     <>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
+      <div> 
+      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,31 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#0b79bf",
+        
+"secondary": "#61419c",
+        
+"accent": "#15d10e",
+        
+"neutral": "#252837",
+        
+"base-100": "#f4f4f4",
+        
+"info": "#afbae9",
+        
+"success": "#1c8644",
+        
+"warning": "#f7dc6e",
+        
+"error": "#ec656b",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
