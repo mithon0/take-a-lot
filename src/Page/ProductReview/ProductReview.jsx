bfcac07a43589792368a42bf6069697f93a0e14d
ovi-khan <manikkhan2240@gmail.com>
2023-10-21 16:15:18 +0600

    redeem gift voucher create

--- src/Page/ProductReview/ProductReview.jsx
+++ src/Page/ProductReview/ProductReview.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { FaCalendar } from 'react-icons/fa';
-import emptyIcon from '../../assets/icons/empty-order-icon.svg-6bcc4601443c5d44b8ae.svg'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { FaCalendar } from "react-icons/fa";
+import emptyIcon from "../../assets/icons/empty-order-icon.svg-6bcc4601443c5d44b8ae.svg";
+import { NavLink } from "react-router-dom";
 
 const ProductReview = () => {
-    return (
-        <div className="w-full mt-0 mt-auto">
+  return (
+    <div className="w-full mt-0 mt-auto">
       <div className="flex justify-between mt-10">
         <div>
           <h2 className="text-lg font-semibold">Product Reviews</h2>
@@ -29,19 +29,43 @@ const ProductReview = () => {
         </div>
       </div>
       <div className="text-center space-y-4 bg-white pt-6 pb-20 mt-4">
-      <div className='justify-center items-center'>
-        <ul className='flex gap-5 ms-5 text-black hover:text-blue-500'>
-            <NavLink to="/account/reviews" className={({ isActive }) => (isActive ? " text-[#5f5f60] text-xs border-b-4 border-blue-500" : "hover:text-blue-500 pl-10 py-2  rounded-r-full bg-opacity-10 border-b-4 border-transparent text-xs black")}>Order Items</NavLink>
-            <NavLink to="/account/reviewshistory" className={({ isActive }) => (isActive ? "text-xs border-b-4 border-blue-500" : "hover:text-blue-500 pl-5 py-2  border-l-4 border-transparent text-xs text-primary")}>Reviews History</NavLink>
-        </ul>
-      </div>
-        <img className="avatar w-32 rounded-full border p-2 shadow-lg" src={emptyIcon} alt="" />
+        <div className="justify-center items-center">
+          <ul className="flex gap-5 ms-5 text-black hover:text-blue-500">
+            <NavLink
+              to="/account/reviews"
+              className={({ isActive }) =>
+                isActive
+                  ? " text-[#5f5f60] text-xs border-b-4 border-blue-500"
+                  : "hover:text-blue-500 pl-10 py-2  rounded-r-full bg-opacity-10 border-b-4 border-transparent text-xs black"
+              }
+            >
+              Order Items
+            </NavLink>
+            <NavLink
+              to="/account/reviewshistory"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-xs border-b-4 border-blue-500"
+                  : "hover:text-blue-500 pl-5 py-2  border-l-4 border-transparent text-xs text-primary"
+              }
+            >
+              Reviews History
+            </NavLink>
+          </ul>
+        </div>
+        <img
+          className="avatar w-32 rounded-full border p-2 shadow-lg"
+          src={emptyIcon}
+          alt=""
+        />
         <p>You have not received any orders for the selected period</p>
         <p>You are only able to review products purchased from Takealot.com</p>
-        <button className="bg-[#0B79BF] text-white px-4 py-2 rounded">Start Shopping</button>
+        <button className="bg-[#0B79BF] text-white px-4 py-2 rounded">
+          Start Shopping
+        </button>
       </div>
     </div>
-    );
+  );
 };
 
 export default ProductReview;
