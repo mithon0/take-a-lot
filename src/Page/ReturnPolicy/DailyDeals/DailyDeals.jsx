aa79506270a8e35cd2a123d4be8ae97a471fd3ef
ovi-khan <manikkhan2240@gmail.com>
2023-10-24 23:23:45 +0600

    daily deals create

--- src/Page/ReturnPolicy/DailyDeals/DailyDeals.jsx
+++ src/Page/ReturnPolicy/DailyDeals/DailyDeals.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React from "react";
 
 const DailyDeals = () => {
-    return (
-        <div>
-            DailyDeals
-        </div>
-    );
+  return (
+    <div className="mx-20 text-xs">
+      <h2 className="text-lg font-bold">Daily Deals Terms and Conditions</h2>
+      <p className="px-6">
+        1. These Daily Deals Terms and Conditions are subject to, and should be
+        read with, our general Terms and Conditions. <br />
+        <br />
+        2. At our discretion, Daily Deals (“Deals”) are available daily from 7am
+        – 23:59pm on week days and from 9am – 23:59pm on weekends. In addition,
+        please note that Daily Deals have a stock limit and may expire earlier
+        if stock runs out. <br /> <br />
+        3. Deal quantities are limited and as such, after a Deal is sold out,
+        those particular Goods may be available on the Website at their normal
+        selling prices (but no longer as a Deal). We do not guarantee a specific
+        saving. The extent of the Deal is at the sole discretion of Takealot.
+        Four of each Daily Deal may be purchased per customer. <br /> <br />
+        4. Adding a Deal to your cart, or completing your order for a Deal
+        without paying for it, does not reserve the item for you. Takealot must
+        receive payment from you within 24 hours of you placing your order for a
+        Deal, otherwise we will cancel your order. <br /> <br />
+        5. Takealot will reserve stock for customers in the order in which it
+        receives payment. Therefore if you pay via EFT, you might not get your
+        item because Takealot may only receive payment from you a few days
+        later, and in the meantime the Deal might sell out to customers paying
+        immediately. <br /> <br />
+        6. The List Price shown in respect of Deals and other discounted Goods
+        is the recommended retail price suggested to us by the supplier of the
+        relevant product. Where the supplier has not provided a recommended
+        retail price, an estimate may be provided. Where a product is offered
+        for sale by a Third Party Seller, the List Price may be provided by the
+        Third Party Seller. <br /> <br />
+        7. By purchasing any Deal, you are also automatically opting in for our
+        Daily Deals daily newsletter as well as our general newsletter (you may
+        opt-out of these newsletters at any time). Opting out of <br /> <br />
+        8. these newsletters after purchase will not affect the value of the
+        Goods purchased. <br /> <br />
+        9. Takealot reserves the right to amend these Terms and Conditions at
+        any time without notice to you.
+      </p>{" "}
+      <br />
+      <h2 className="text-lg font-bold">App Only Deals Terms and Conditions</h2>
+      <p className="px-6">
+        We also have Deals (called “App Only Deals”) which will only be
+        available for purchase using the Takealot software application (“App”),
+        although they may also be displayed on the Takealot website and
+        mobi-site. The App can be downloaded on IOS and Android devices. In
+        addition to the above Daily Deal Terms and Conditions, the following
+        terms apply to App Only Deals: <br /> <br />
+        1. App Only Deals are only available for purchase using the App and
+        their prices may differ from the normal selling prices at which those
+        particular Goods may be available on the website or mobi-site. <br />{" "}
+        <br />
+        2. You will only receive the App Only Deal price if you pay for your
+        order using the App before the App Only Deal has expired or sells out.
+        You will not receive the App Only Deal price if you pay for your order
+        using the website or mobi-site, or if you don’t pay for your order
+        before the App Only Deal has expired or sells out.
+      </p>{" "}
+      <br />
+    </div>
+  );
 };
 
 export default DailyDeals;
