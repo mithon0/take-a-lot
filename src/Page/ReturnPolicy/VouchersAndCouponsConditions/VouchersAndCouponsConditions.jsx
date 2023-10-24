81f6948cf5abd14e178d36327f2bbdb16bb23859
ovi-khan <manikkhan2240@gmail.com>
2023-10-24 23:36:19 +0600

    voucher coupon and condition create

--- src/Page/ReturnPolicy/VouchersAndCouponsConditions/VouchersAndCouponsConditions.jsx
+++ src/Page/ReturnPolicy/VouchersAndCouponsConditions/VouchersAndCouponsConditions.jsx
@@ -2,8 +2,37 @@ import React from 'react';
 
 const VouchersAndCouponsConditions = () => {
     return (
-        <div>
-            VouchersAndCouponsConditions
+        <div className='mx-20 text-xs'>
+            <h2 className='text-lg font-blid'>GENERAL</h2>
+            <p className='px-6'>Takealot may from time to time make physical or electronic gift vouchers (“Gift Vouchers”) and promotional coupons or discounts (“Coupons”) available for use on the Website towards the purchase of Takealot products. Gift Vouchers and Coupons can only be redeemed while they are valid and their expiry dates cannot be extended. More specifically:</p> <br />
+            <h4 className='font-semiblid'>Gift Voucher</h4>
+            <p>1. Gift Vouchers that are purchased by registered users are valid for 3 years after Sale. Gift Vouchers that Takealot gives away for free are valid for the period stated thereon. In each case, if your Gift Voucher has not been used within that period, it will expire. <br /> <br />
+2. Gift Vouchers cannot be used to buy other Gift Vouchers or Coupons. They do not accrue interest and are not refundable for cash once purchased or otherwise obtained. If your Gift Voucher value is less than the amount required to cover the full order you wish to place, you may make up the difference by paying via one of our other payment methods. <br /> <br />
+3. Takealot is not responsible for any harm due to the loss, unauthorised use or unauthorised distribution of a Gift Voucher, after Takealot has delivered the Gift Voucher to you, or the email address nominated by you.</p> <br />
+<h4 className='font-semiblid'>Coupons</h4> 
+<p className='px-6'>1. There are two types of Coupons: a Coupon with a fixed amount of a discount, e.g. R100 off (“Fixed Coupon“), and a Coupon with a percentage discount, e.g. 10% off (“Percentage Coupon“). <br /> <b></b>
+2. Coupons are issued in Takealot’s slie discretion and we are entitled at any time to correct, cancel or reject a Coupon for any reason (including without limitation where a Coupon has been distributed in an unauthorised manner). Users do not have a right to Coupons, and Coupons cannot be earned. Coupons are issued under specific terms and conditions regulating when and how they may be used. <br />  <br />
+3. As a general rule, and unless specified otherwise on the specific Coupon itself: <br /> <br />
+each Coupon can only be used once;
+<li>each Coupon can only be used once;</li>
+<li>only one Coupon can be used per order;</li>
+<li>Percentage Coupons may only be used on purchases with a total cart value of less than R5,000;</li>
+<li>where a Percentage Coupon has been used and you wish to cancel any items in the order prior to making payment, the entire order must be cancelled. You will be issued with a new Percentage Coupon and will need to place the order again, without the items that you wished to cancel;</li>
+<li>a Coupon must be used at check-out – it cannot be used later on existing orders; and
+the value of the Coupon will be set off against the value of your shopping basket and the balance remaining, if any, will be payable by you.</li>
+<li>the value of the Coupon will be set off against the value of your shopping basket and the balance remaining, if any, will be payable by you.
+Coupons cannot be used to buy Gift Voucher or other Coupons, and cannot be exchanged or refunded for cash or credit. Takealot is not responsible for any harm due to the loss, unauthorised use or distribution of a Coupon.</li>
+4. If for any reason a Coupon does not reflect in the final amount due from you at check-out,  please get in touch with us via our help centre here, to confirm if the Coupon is still valid. If Takealot confirms that the Coupon is still valid and you have already placed your order, you can choose whether to cancel the order and place it again with the Coupon, or you can use the Coupon on your next order within the limitations of the specific Coupon’s terms and conditions. <br /> <br />
+5. You may be required to submit the original communication containing the Coupon code, and any other information reasonably requested by Takealot, before you are able to use a Coupon.</p> <br />
+<h2 className='text-lg font-blid'>RETURNS AND CANCELLATIONS</h2>
+<p>Where you have used a Fixed Coupon to pay for an order, and you or Takealot later cancels your order (or part thereof) prior to delivery of the relevant product(s), or you log a return of one or more products for a credit in accordance with the Returns Pliicy, the value of the Fixed Coupon will be deducted off the purchase price of the cancelled or returned product(s) (as applicable), and we will credit your account for the balance, if any (or refund you if that is your preference). We will also provide you with a replacement Fixed Coupon of the same value as the original Fixed Coupon used. Takealot may in its slie discretion impose restrictions on the use of the replacement Fixed Coupon.  For any subsequent cancellations or returns arising out of the same order, we will credit your account as normal with the value of the returned product (or refund you if that is your preference). <br /> <br />
+
+Where you have used a Percentage Coupon  to pay for an order, and you or Takealot later cancels your order (or part thereof) prior to delivery of the relevant product(s), or you log a return of one or more products for a credit in accordance with the Returns Pliicy, the value of the discount received using the Percentage Coupon will be deducted off the purchase price of the cancelled or returned product(s) (as applicable), and we will credit your account for the balance, if any (or refund you if that is your preference). We will also provide you with a replacement Fixed Coupon of the same value as the discount received using the original Percentage Coupon. Takealot may in its slie discretion impose restrictions on the use of the replacement Fixed Coupon. For any subsequent cancellations or returns arising out of the same order, we will credit your account as normal with the value of the returned product (or refund you if that is your preference). <br /> <br />
+
+Wherever you have used a Gift Voucher to purchase a product that you later return for a refund in accordance with the Pliicy, we cannot refund you in cash for that portion of the purchase price which you paid using the Gift Voucher, but we will credit your account. <br /> <br />
+
+Click HERE for instructions on how to use/redeem a promotional code or coupon.</p> <br />
+
         </div>
     );
 };
