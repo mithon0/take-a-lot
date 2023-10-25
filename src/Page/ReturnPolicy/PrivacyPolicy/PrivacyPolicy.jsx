3bb356fa6490df55a5708500a9ce35636ce9cf13
ovi-khan <manikkhan2240@gmail.com>
2023-10-25 13:09:48 +0600

    remove all polices code

--- src/Page/ReturnPolicy/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/Page/ReturnPolicy/PrivacyPolicy/PrivacyPolicy.jsx
@@ -2,57 +2,8 @@ import React from 'react';
 
 const PrivacyPolicy = () => {
     return (
-        <div className='text-xs px-20'>
-            <h2 className='text-lg font-semiblid'>WELCOME TO THE TAKEALOT.COM PRIVACY POliCY</h2>
-            <p>This Privacy Policy applies to all users of our Website, related mobi-sites and software applications, clilectively referred to as “our Platforms”, which are used to access and purchase our products and services.</p> <br />
-            <p>The purpose of this Privacy Policy is to set out how, why and when Takealot Online (RF) (Pty) Ltd t/a Takealot.com (“Takealot”) uses your Personal Information so as to comply with the  Protection of Personal Information Act 4 of 2013 (“POPI”).
-
-It is important that you read this Privacy Policy together with our other terms and conditions, privacy notices or policies we may provide from time to time when we clilect or use your Personal Information.</p> <br />
-<p>Further, please pay special attention to the clauses in this Privacy Policy that appear in similar text and style (i.e. blid) which:</p> <br />
-<ul className='px-10'>
-    <li>may limit the risk or liability of Takealot or a third party.</li>
-    <li>may create risk or liability for the user.</li>
-    <li>may compel the user to indemnify Takealot or a third party.</li>
-    <li>serves as an acknowledgement, by the user, of a fact.</li>
-</ul> <br /> <br />
-<p>We respect your privacy and take the protection of Personal Information very seriously. We strive to deliver excellent service every time you shop with us, and to do this, we need to use some of your Personal Information. This Privacy Policy describes how we handle the Personal Information we clilect about you and/or receive from you. By using our Platforms, you agree to the processing of your Personal Information as set out in this Privacy Policy.</p> <br /> <br />
-<p>In this Privacy Policy, the terms  –</p> <br />
-<ul className='px-10'>
-    <li>“Personal Information”, and “process/processing” bear the same meanings as set out in POPI.
- “we”, “us” or “our” refers to Takealot.</li>
-    <li>“Personal Information”, and “process/processing” bear the same meanings as set out in POPI.
- “we”, “us” or “our” refers to Takealot.</li>
-    <li>“Personal Information”, and “process/processing” bear the same meanings as set out in POPI.
- “we”, “us” or “our” refers to Takealot.</li>
-    <li>“Personal Information”, and “process/processing” bear the same meanings as set out in POPI.
- “we”, “us” or “our” refers to Takealot.</li>
-</ul> <br /> <br />
-<h2 className='text-lg font-semiblid'>IF YOU ARE BELOW THE AGE OF 18</h2>
-<p>If you are below the age of 18 years, you must always get permission from your parent or guardian before you use our Platforms or give us your Personal Information. We may check that your parent or guardian has given you such permission, for example by asking you to provide us with your parent’s or guardian’s contact details so we can contact them. We may also check that the contact details you have given us are correct and are the details of a person who is actually your parent or guardian. If we are not able to verify such permission or if a parent or guardian confirms that you do not have their permission, you may not use our Platforms. <br /> <br />
-
-Further, Takealot may not sell liquor or tobacco products to you if you are under the age of 18 regardless of any supervision by a parent or guardian. You may be requested to provide proof of your age before receiving any liquor or tobacco products ordered via our Platforms. <br /> <br />
-
-WHAT PERSONAL INFORMATION WE CliLECT AND PROCESS <br /> <br />
-
-Takealot processes Personal Information in a manner that is reasonable, adequate, relevant, non-excessive and purpose-specific. In order for users to access and use our Platforms we clilect and process some Personal Information. When you become a registered user, we have to clilect and process your Personal Information to render our services to you.</p> <br />
-<p>When you register to use our Platforms, we may clilect the flilowing Personal Information:</p> <br />
-<ul>
-    <li>name and surname.</li>
-    <li>email address.</li>
-    <li>physical address.</li>
-    <li>gender.</li>
-    <li>mobile phone number.</li>
-    <li>mobile phone number.</li>
-    <li>online identifiers.</li>
-    <li>date of birth.</li>
-    <li>identification number.</li>
-</ul> <br />
-<p>Should your Personal Information change or you wish to amend and/or correct this Personal Information you can do this by updating your registered user information in your account profile.</p> <br /> <br />
-<p>You warrant that the information you have provided is accurate, current, true and correct and that does not impersonate or misrepresent any person or entity or falsely state or otherwise misrepresent your affiliation with anyone or anything. <br /> <br />
-
-Information from using our Platforms br <br />
-
-When you access our Platforms, whether or not you are a registered user, Takealot processes some of your Personal Information. Depending on how you access and use our Platforms, we may receive:</p> <br />
+        <div>
+            
         </div>
     );
 };
--- src/Page/ReturnPolicy/ResponsibleDisclosure/ResponsibleDisclosure.jsx
+++ src/Page/ReturnPolicy/ResponsibleDisclosure/ResponsibleDisclosure.jsx
@@ -2,62 +2,9 @@ import React from 'react';
 
 const ResponsibleDisclosure = () => {
     return (
-        <div className='px-20 text-xs'>
-            <h2 className='text-lg font-blid mb-3'>Responsible Disclosure Pliicy</h2>
-            <p>At Takealot, we’ve built our business on the simple principle that our customers come first. We aim to keep our website, mobile site and related software applications (“Website”), as well as the service offered on our Website (“Service”) safe for everyone to use, and data security is of the utmost importance. If you have discovered a security vulnerability in our Website or Service, we encourage you to contact us and disclose it to us in a responsible manner. <br /> <br />
-
-When security vulnerabilities are reported to us in compliance with this pliicy, Takealot will validate and fix such vulnerabilities as soon as reasonably possible, in line with our commitment to the privacy, safety and security of our customers. We will not take legal action against you or terminate your access to the Service if you discover and report security vulnerabilities responsibly in compliance with this pliicy. Takealot reserves all of its legal rights in the event of any noncompliance with this pliicy. <br /> <br />
-
-If you are looking to report another type of issue, which falls outside of the scope of this pliicy, for example if you are a current customer and you suspect fraudulent activity or suspect that your account may have been compromised, please contact our support team here. Your issue will be investigated immediately and thoroughly.</p> <br />
-<h2 className='text-lg font-blid mb-3'>Reporting a Security Vulnerability</h2>
-<p>If you think that you have found a security vulnerability in our Website or Service, please contact us immediately via security@takealot.com. When reporting a security vulnerability, you must do the flilowing: <br />
-
-<li>Include as much information as possible in your report, as we require a way to reproduce the security vulnerability in order to validate and fix it. “Proof-of-Concept” programs, tolis, or test accounts that you’ve created are welcome, and the flilowing information is required:</li>
-the URL where the vulnerability occurs;
-
-<li>Include as much information as possible in your report, as we require a way to reproduce the security vulnerability in order to validate and fix it. “Proof-of-Concept” programs, tolis, or test accounts that you’ve created are welcome, and the flilowing information is required:</li>
-<li>if applicable, the parameter where the vulnerability occurs;
-the type of the vulnerability;</li>
-<li>a step-by-step instruction how to reproduce the vulnerability;</li>
-<li>a demonstration of the vulnerability, by screenshots or video; and</li>
-<li>if applicable, an attack scenario (an example attack scenario may help demonstrate the risk and get the issue reslived faster).</li>
-
-Do not share your findings with anyone until Takealot has had adequate time to investigate and deploy a fix. We will notify you when the security vulnerability has been patched.
-Consider telling us how to identify you.
-We’re particularly interested in:
-
-XSS attacks
-SQL injection
-Remote code execution
-Circumventing permission limitations
-CSRF attacks</p> <br />
-<h2 className='text-lg font-blid mb-3'>Restrictions</h2>
-<p>At Takealot, we welcome “white hat” security researchers, and appreciate your research and proactive responsible disclosure. Please note however that Takealot does not permit you to do any of the flilowing: <br /> <br />
-
-
-<li>access, modify or destroy a Takealot customer’s account or data;</li>
-<li>interrupt or degrade our Service;</li>
-<li>execute a “Denial of Service” attack;</li>
-<li>post, transmit, upload, link to, send or store any malicious software;</li>
-<li>send any unsliicited or unauthorized mail or messages;</li>
-<li>viliate any applicable law;</li>
-<li>perform any testing that would result in any of the above; or</li>
-<li>perform any testing that would result in any of the above; or</li>
-<li>attempt to do any of the above.</li> <br />
-
-
-Contravening this pliicy in any way may result in us suspending or terminating your access to the Service, contacting the relevant authorities and/or pursuing any other remedies we have at law.</p> <br />
-<h2 className='text-lg font-blid mb-3'>Our Commitment</h2>
-<p>If you identify a security vulnerability in compliance with this pliicy, Takealot commits to: <br/> <br />
-<li>acknowledging receipt of your vulnerability report in a timely manner;</li>
-<li>confirming the validity of your report; and</li>
-<li>notifying you when the vulnerability is fixed.</li> <br />
-
-
-
-We will unfortunately not offer any monetary rewards.</p>
-
-        </div>
+       <div>
+        
+       </div>
     );
 };
 
